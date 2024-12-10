137914f754f1fc9dac35423649a812448021e370
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-10 14:08:43 +0900

    feat(domain) : Pair
    - equals 오버라이딩, 순서 관계 없이 같은 패어를 찾음

--- src/main/java/pairmatching/domain/Pair.java
+++ src/main/java/pairmatching/domain/Pair.java
@@ -1,4 +1,25 @@
 package pairmatching.domain;
 
 public class Pair {
+    private String first;
+    private String second;
+
+    public Pair(String first, String second) {
+        this.first = first;
+        this.second = second;
+    }
+    public String getFirst() {
+        return first;
+    }
+    public String getSecond() {
+        return second;
+    }
+    @Override
+    public boolean equals(Object o) {
+        boolean equals = false;
+        if (o instanceof Pair) {
+            equals = first.equals(((Pair) o).first) && second.equals(((Pair) o).second) || first.equals(((Pair) o).second) && second.equals(((Pair) o).first);
+        }
+        return equals;
+    }
 }
