bf93317acab548321282519edf84152c405067d2
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-10 14:00:55 +0900

    fix(util) : File_InputView
    - 생성자 구분, 생성자 별 파일 경로 업데이트

--- src/main/java/pairmatching/view/FileInputView.java
+++ src/main/java/pairmatching/view/FileInputView.java
@@ -13,9 +13,15 @@ import pairmatching.util.NUMBER_CONSTANT;
 
 
 public class FileInputView {
-    // Util의 File Path
-    static final String filePath = FILE_CONSTANT.productFilePath.toString();
+    static String filePath = "";
 
+    public FileInputView(int courseIndex) {
+        if (courseIndex == 0) {
+            filePath = FILE_CONSTANT.BACKEND_FILE_PATH.toString();
+        } else {
+            filePath = FILE_CONSTANT.FRONTEND_FILE_PATH.toString();
+        }
+    }
     // 파일을 읽어서 버퍼로 반환
     private static BufferedReader readFileToBuffer(String filePath) {
         File file = new File(filePath);
@@ -28,12 +34,11 @@ public class FileInputView {
         return new BufferedReader(fileReader);
     }
 
-    // 첫 번째 줄을 제외하고
+
     // 버퍼를 한줄씩 읽어서 문자열로 반환
     // 이때, fileDelimiter 기준으로 구분하여 저장 -> Scanner의 nextLine()으로 불러올 수 있다.
-    private static String fileToString(BufferedReader bufferedReader) {
-        return bufferedReader.lines()
-                .skip(NUMBER_CONSTANT.ONE.getLong())
+    public static String fileToString() {
+        return readFileToBuffer(filePath).lines()
                 .collect(Collectors.joining(FILE_CONSTANT.fileDelimiter.toString()));
     }
 
