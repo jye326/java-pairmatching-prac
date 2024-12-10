0c05d5039955bfabdda84e16967e1058c3a43914
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-10 13:29:46 +0900

    feat(domain) : Crew
    - 멤버
    - getter setter

--- src/main/java/pairmatching/domain/Crew.java
+++ src/main/java/pairmatching/domain/Crew.java
@@ -1,4 +1,22 @@
 package pairmatching.domain;
 
 public class Crew {
+    private Course course;
+    private String name;
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
