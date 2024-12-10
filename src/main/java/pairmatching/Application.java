cd5531faa475e683207894008f3a6738704d8efc
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-10 23:50:00 +0900

    feat(app) : Application
    - container 객체 생성
    - 진행

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,14 @@
 package pairmatching;
 
+import pairmatching.controller.PairMatchingController;
+import pairmatching.domain.Pair;
+import pairmatching.domain.PairHistory;
+import pairmatching.util.OUTPUT_MESSAGE;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+        PairMatchingController p = new PairMatchingController();
+        p.start();
     }
 }
