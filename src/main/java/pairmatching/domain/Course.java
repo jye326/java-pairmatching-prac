2366c169418823b7b200d86f4251f362e5612242
JuYongeun <65019047+jye326@users.noreply.github.com>
2024-12-10 13:59:40 +0900

    feat(domain) : Course
    - 속성 추가 : index, crewNameList
    - 함수 추가 : 파일을 읽어 crewNameList를 업데이트

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,4 +1,45 @@
 package pairmatching.domain;
 
-public class Course {
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+import pairmatching.util.FILE_CONSTANT;
+import pairmatching.view.FileInputView;
+
+public enum Course {
+    BACKEND("백엔드", 0),
+    FRONTEND("프론트엔드", 1);
+
+    private String name;
+    private int index;
+    private ArrayList<String> crewNameList = new ArrayList<>();
+
+    Course(String name, int index) {
+        this.name = name;
+        this.index = index;
+        // 여기서 벡엔드 파일 읽어오는 함수 호출해서 crewList 채워넣기
+        updateCrewList();
+    }
+
+    private void updateCrewList() {
+        String crewListString = new FileInputView(index).fileToString();
+        Scanner scanner = new Scanner(crewListString);
+        scanner.useDelimiter(FILE_CONSTANT.fileDelimiter.toString());
+        while (scanner.hasNextLine()) {
+            String crewName = scanner.nextLine();
+            crewNameList.add(crewName);
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void printCrewNameList() {
+        for (String crewName : crewNameList) {
+            System.out.println(crewName);
+        }
+    }
+
 }
